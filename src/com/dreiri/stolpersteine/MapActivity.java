9172969d882757cb61879de275f35d273ee2c62b
Jing Li <thyrlian@gmail.com>
2013-07-12 06:50:15 +0200

    Initial setup

---
+++ src/com/dreiri/stolpersteine/MapActivity.java
@@ -0,0 +1,23 @@
+
+package com.dreiri.stolpersteine;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.map, menu);
+        return true;
+    }
+
+}
