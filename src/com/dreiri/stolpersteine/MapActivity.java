19e895f9449911f6191ad4ae904f38ce4540cc38
Jing Li <thyrlian@gmail.com>
2013-07-12 11:02:24 +0200

    Added map fragment to activity_map layout, and removed default ActionItem settings

--- src/com/dreiri/stolpersteine/MapActivity.java
+++ src/com/dreiri/stolpersteine/MapActivity.java
@@ -1,9 +1,8 @@
 
 package com.dreiri.stolpersteine;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class MapActivity extends Activity {
 
@@ -11,13 +10,6 @@ public class MapActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.map, menu);
-        return true;
     }
 
 }
