571106cc446176785ed5995545d560f7b233ca86
Jing Li <thyrlian@gmail.com>
2013-07-16 22:43:57 +0200

    First working version

--- src/com/dreiri/stolpersteine/MapActivity.java
+++ src/com/dreiri/stolpersteine/MapActivity.java
@@ -1,25 +1,42 @@
 
 package com.dreiri.stolpersteine;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.dreiri.stolpersteine.utils.Client;
+import com.dreiri.stolpersteine.utils.JSONResponsible;
 import com.dreiri.stolpersteine.utils.LocationFinder;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends Activity {
     GoogleMap map;
+    LatLng berlin = new LatLng(52.5191710, 13.40609120);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_map);
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragmentMap)).getMap();
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(berlin, 12));
+        map.setOnInfoWindowClickListener(new InfoWindowHandler());
+        String uri = "https://stolpersteine-api.eu01.aws.af.cm/v1/stolpersteine?offset=0&limit=1";
+        Client client = new Client();
+        client.getJSONFeed(uri, new JSONResponseHandler());
     }
     
     @Override
@@ -42,6 +59,39 @@ public class MapActivity extends Activity {
                 break;
         }
         return true;
+    }
+    
+    private class JSONResponseHandler implements JSONResponsible {
+
+        @Override
+        public void actOnJSON(JSONArray jsonArray) {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject;
+                try {
+                    jsonObject = jsonArray.getJSONObject(i);
+                    JSONObject person = jsonObject.getJSONObject("person");
+                    JSONObject location = jsonObject.getJSONObject("location");
+                    JSONObject coordinates = location.getJSONObject("coordinates");
+                    String name = person.getString("firstName") + " " + person.getString("lastName");
+                    String address = location.getString("street") + ", " + location.getString("zipCode") + " " + location.getString("city");
+                    LatLng position = new LatLng(coordinates.getDouble("latitude"), coordinates.getDouble("longitude"));
+                    MarkerOptions markerOptions = new MarkerOptions().position(position).title(name).snippet(address).icon(BitmapDescriptorFactory.fromResource(R.drawable.stolpersteine_tile)); 
+                    map.addMarker(markerOptions);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+    }
+    
+    private class InfoWindowHandler implements OnInfoWindowClickListener {
+
+        @Override
+        public void onInfoWindowClick(Marker marker) {
+            
+        }
+        
     }
 
 }
