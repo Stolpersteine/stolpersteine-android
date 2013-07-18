2b0260c29708c86ca9e61c71866d0fe878166f19
Jing Li <thyrlian@gmail.com>
2013-07-19 00:15:09 +0200

    Added InfoActivity.

---
+++ src/com/dreiri/stolpersteine/InfoActivity.java
@@ -0,0 +1,23 @@
+package com.dreiri.stolpersteine;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class InfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_info);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.info, menu);
+        return true;
+    }
+
+}
