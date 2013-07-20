85ef9f188de8d2386fadf29d74f2e56c79b73534
Jing Li <thyrlian@gmail.com>
2013-07-20 13:28:39 +0200

    Person class now implements Parcelable.

--- src/com/dreiri/stolpersteine/models/Person.java
+++ src/com/dreiri/stolpersteine/models/Person.java
@@ -1,19 +1,26 @@
 package com.dreiri.stolpersteine.models;
 
-public class Person {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Person implements Parcelable {
 
     private String firstName;
     private String lastName;
     private String biography;
     
-    public Person(String firstName, String lastName) {
-        new Person(firstName, lastName, "");
-    }
-    
     public Person(String firstName, String lastName, String biography) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.biography = biography;
+    }
+    
+    public Person(String firstName, String lastName) {
+        new Person(firstName, lastName, "");
+    }
+    
+    public Person(Parcel orig) {
+        readFromParcel(orig);
     }
 
     public String getFirstName() {
@@ -43,5 +50,36 @@ public class Person {
     public String name() {
         return firstName + " " + lastName;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.firstName);
+        dest.writeString(this.lastName);
+        dest.writeString(this.biography);
+    }
+    
+    private void readFromParcel(Parcel orig) {
+        this.firstName = orig.readString();
+        this.lastName = orig.readString();
+        this.biography = orig.readString();
+    }
+    
+    public static final Parcelable.Creator<Person> CREATOR = new Parcelable.Creator<Person>() {
+
+        @Override
+        public Person createFromParcel(Parcel source) {
+            return new Person(source);
+        }
+
+        @Override
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
 
 }
