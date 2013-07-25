f91d89b108d7aec2a45b9ebab90eb2e30cc37542
Jing Li <thyrlian@gmail.com>
2013-07-25 07:42:21 +0200

    Location class now implements Parcelable

--- src/com/dreiri/stolpersteine/models/Location.java
+++ src/com/dreiri/stolpersteine/models/Location.java
@@ -2,21 +2,24 @@ package com.dreiri.stolpersteine.models;
 
 import java.util.ArrayList;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.maps.model.LatLng;
 
-public class Location {
+public class Location implements Parcelable {
 
     private String street;
     private String zipCode;
     private String city;
-    private ArrayList<String> sublocalities;
+    private ArrayList<String> sublocalities = new ArrayList<String>();
     private LatLng coordinates; 
     
     public Location(String street, String zipCode, String city, ArrayList<String> sublocalities, LatLng coordinates) {
         this.street = street;
         this.zipCode = zipCode;
         this.city = city;
-        this.sublocalities = sublocalities;
+        this.sublocalities.addAll(sublocalities);
         this.coordinates = coordinates;
     }
     
@@ -30,16 +33,20 @@ public class Location {
         new Location(street, zipCode, city, sublocalities, coordinates);
     }
     
+    public Location(String street, String zipCode, String city, LatLng coordinates) {
+        new Location(street, zipCode, city, new ArrayList<String>(), coordinates);
+    }
+    
     public Location(String street, String zipCode, String city, ArrayList<String> sublocalities) {
         new Location(street, zipCode, city, sublocalities, null);
     }
     
-    public Location(String street, String zipCode, String city, LatLng coordinates) {
-        new Location(street, zipCode, city, null, coordinates);
+    public Location(String street, String zipCode, String city) {
+        new Location(street, zipCode, city, new ArrayList<String>(), null);
     }
     
-    public Location(String street, String zipCode, String city) {
-        new Location(street, zipCode, city, null, null);
+    public Location(Parcel orig) {
+        readFromParcel(orig);
     }
     
     public String getStreet() {
@@ -71,7 +78,8 @@ public class Location {
     }
 
     public void setSublocalities(ArrayList<String> sublocalities) {
-        this.sublocalities = sublocalities;
+        this.sublocalities.clear();
+        this.sublocalities.addAll(sublocalities);
     }
 
     public LatLng getCoordinates() {
@@ -97,5 +105,42 @@ public class Location {
     public String address() {
         return this.street + ", " + this.zipCode + " " + this.city;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.street);
+        dest.writeString(this.zipCode);
+        dest.writeString(this.city);
+//        dest.writeSerializable(this.sublocalities);
+        dest.writeList(this.sublocalities);
+        dest.writeParcelable(this.coordinates, 0);
+    }
+    
+    private void readFromParcel(Parcel orig) {
+        this.street = orig.readString();
+        this.zipCode = orig.readString();
+        this.city = orig.readString();
+//        this.sublocalities = orig.readArrayList(String.class.getClassLoader());
+        orig.readList(this.sublocalities, getClass().getClassLoader());
+        this.coordinates = orig.readParcelable(LatLng.class.getClassLoader());
+    }
+    
+    public static final Parcelable.Creator<Location> CREATOR = new Parcelable.Creator<Location>() {
+
+        @Override
+        public Location createFromParcel(Parcel source) {
+            return new Location(source);
+        }
+
+        @Override
+        public Location[] newArray(int size) {
+            return new Location[size];
+        }
+    };
 
 }
