59cc995775a991cb4997780f28c2398f623a01c1
Jing Li <thyrlian@gmail.com>
2013-07-26 09:30:02 +0200

    Only load necessary div in WebView

--- src/com/dreiri/stolpersteine/BioActivity.java
+++ src/com/dreiri/stolpersteine/BioActivity.java
@@ -1,8 +1,15 @@
 package com.dreiri.stolpersteine;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.webkit.WebView;
 
 public class BioActivity extends Activity {
@@ -15,9 +22,39 @@ public class BioActivity extends Activity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_web);
         Intent intent = getIntent();
-        String bioUrl = intent.getStringExtra("bioUrl");
+        final String bioUrl = intent.getStringExtra("bioUrl");
         browser = (WebView) findViewById(R.id.webkit);
-        browser.loadUrl(bioUrl);
+        browser.getSettings().setBuiltInZoomControls(true);
+        browser.getSettings().setDisplayZoomControls(false);
+        
+        Thread downloadThread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    Document document = Jsoup.connect(bioUrl).get();
+//                    Elements elements = document.select("div.panel-pane pane-entity-view pane-person");
+//                    Elements elements = document.select("div.panel-content");
+                    Elements elements = document.select("div#biografie_seite");
+                    String data = elements.toString();
+                    String mimeType = "text/html";
+                    String encoding = "utf-8";
+                    browser.loadData(data, mimeType, encoding);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    browser.loadUrl(bioUrl);
+                }
+            }
+        };
+        
+        downloadThread.start();
     }
     
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bio, menu);
+        return true;
+    }
+
 }
