5ae008be66ea2d8b63bbfe949e1cf041b9eb542a
Jing Li <thyrlian@gmail.com>
2013-10-31 15:10:07 +0100

    Remove import of Log

--- src/com/dreiri/stolpersteine/activities/MapActivity.java
+++ src/com/dreiri/stolpersteine/activities/MapActivity.java
@@ -7,7 +7,6 @@ import org.csdgn.util.KDTree;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
