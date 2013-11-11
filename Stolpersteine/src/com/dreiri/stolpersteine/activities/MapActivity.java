e0785a2228c7eee6cbb5aa43d0567c2f830b995f
Claus Höfele <claus@claushoefele.com>
2013-11-11 20:53:06 +0100

    Disable action bar in map activity

--- Stolpersteine/src/com/dreiri/stolpersteine/activities/MapActivity.java
+++ Stolpersteine/src/com/dreiri/stolpersteine/activities/MapActivity.java
@@ -14,6 +14,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -50,6 +51,7 @@ public class MapActivity extends Activity implements OnInfoWindowClickListener, 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_map);
 		
 		// Set up map and clustering
@@ -104,7 +106,7 @@ public class MapActivity extends Activity implements OnInfoWindowClickListener, 
 			}
 		});
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
