1661edf054adbdd7f24dca91fb029f6c1c740eb7
Claus Höfele <choefele@ebay.com>
2013-11-19 14:39:00 +0100

    Testing basic auth

--- Stolpersteine/src/com/dreiri/stolpersteine/api/NetworkService.java
+++ Stolpersteine/src/com/dreiri/stolpersteine/api/NetworkService.java
@@ -2,6 +2,8 @@ package com.dreiri.stolpersteine.api;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 import java.net.ResponseCache;
 import java.net.URL;
 
@@ -11,9 +13,12 @@ import android.util.Log;
 
 import com.squareup.okhttp.HttpResponseCache;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.internal.http.HttpAuthenticator;
 
 public class NetworkService {
-    private static final String BASE_URL = "https://stolpersteine-api.eu01.aws.af.cm/v1";
+    private static final String API_BASE_URL = "https://stolpersteine-api.eu01.aws.af.cm/v1";
+    private static final String API_CLIENT_USER = "android";
+    private static final String API_CLIENT_PASSWORD = "test";
     private static final int CACHE_SIZE_BYTES = 1024 * 1024;
     private SearchData defaultSearchData = new SearchData();
     OkHttpClient httpClient = new OkHttpClient();
@@ -26,6 +31,15 @@ public class NetworkService {
         } catch (IOException e) {
             Log.e("Stolpersteine", "Error creating disk cache", e);
         }
+        
+        // Authentication
+        Authenticator.setDefault(new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(API_CLIENT_USER, API_CLIENT_PASSWORD.toCharArray());
+            }
+        });
+        OkAuthenticator authenticator = HttpAuthenticator.SYSTEM_DEFAULT; 
+        httpClient.setAuthenticator(authenticator);
     }
 
     public SearchData getDefaultSearchData() {
@@ -37,7 +51,7 @@ public class NetworkService {
     }
 
     public void retrieveStolpersteine(SearchData searchData, int offset, int limit, RetrieveStolpersteineRequest.Callback callback) {
-        URL url = RetrieveStolpersteineRequest.buildQuery(BASE_URL, searchData, defaultSearchData, offset, limit);
+        URL url = RetrieveStolpersteineRequest.buildQuery(API_BASE_URL, searchData, defaultSearchData, offset, limit);
         RetrieveStolpersteineRequest task = new RetrieveStolpersteineRequest(httpClient, callback);
         task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, url);
     }
