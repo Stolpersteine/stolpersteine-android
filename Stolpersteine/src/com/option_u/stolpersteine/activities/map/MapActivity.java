111cee338e2592c45ce4eb3c1e1e3f6aad1dd820
Jing Li <thyrlian@gmail.com>
2014-04-24 21:06:40 +0200

    Corrected formatting.

--- Stolpersteine/src/com/option_u/stolpersteine/activities/map/MapActivity.java
+++ Stolpersteine/src/com/option_u/stolpersteine/activities/map/MapActivity.java
@@ -25,94 +25,94 @@ import com.option_u.stolpersteine.api.SynchronizationController;
 import com.option_u.stolpersteine.api.model.Stolperstein;
 
 public class MapActivity extends Activity implements SynchronizationController.Listener, ClusterManager.OnClusterClickListener<Stolperstein>, ClusterManager.OnClusterItemClickListener<Stolperstein> {
-    
-	private SynchronizationController synchronizationController;
-	private ClusterManager<Stolperstein> clusterManager;
-	private LocationService locationService;
+
+    private SynchronizationController synchronizationController;
+    private ClusterManager<Stolperstein> clusterManager;
+    private LocationService locationService;
     private GoogleMap map;
     private Menu menu;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-		
-		// Set up map and clustering
-		map = ((MapFragment)getFragmentManager().findFragmentById(R.id.fragmentMap)).getMap();
-		if (map != null) {
-		    map.getUiSettings().setZoomControlsEnabled(false);
-		    map.getUiSettings().setZoomGesturesEnabled(true);
-		    
-		    // Clustering
-		    clusterManager = new ClusterManager<Stolperstein>(this, map);
-//		    clusterManager.setAlgorithm(new GridBasedAlgorithm<Stolperstein>());
-	        clusterManager.setRenderer(new ClusterRenderer(this, map, clusterManager));
-	        clusterManager.setOnClusterClickListener(this);
-	        clusterManager.setOnClusterItemClickListener(this);
-		    map.setOnCameraChangeListener(clusterManager);
-		    map.setOnMarkerClickListener(clusterManager);
-		    
-		    // User location
-	        locationService = new LocationService(this, map, R.array.berlin);
-	        locationService.zoomToRegion(false);
-		}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
 
-		// Start synchronizing data
-		StolpersteineNetworkService networkService = new StolpersteineNetworkService(this);
-		networkService.getDefaultSearchData().setCity("Berlin");
-		synchronizationController = new SynchronizationController(networkService);
-		synchronizationController.setListener(this);
-		synchronizationController.synchronize();		
-	}
-	
-	private boolean isLocationOption() {
-	    MenuItem menuItem = menu.findItem(R.id.action_positioning);
-	    String locationTitle = getResources().getString(R.string.map_action_location);
-	    return menuItem.getTitle().equals(locationTitle);
-	}
-	
-	private void toggleOption() {
-	    int title, drawable;
-	    if (isLocationOption()) {
+        // Set up map and clustering
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragmentMap)).getMap();
+        if (map != null) {
+            map.getUiSettings().setZoomControlsEnabled(false);
+            map.getUiSettings().setZoomGesturesEnabled(true);
+
+            // Clustering
+            clusterManager = new ClusterManager<Stolperstein>(this, map);
+            // clusterManager.setAlgorithm(new GridBasedAlgorithm<Stolperstein>());
+            clusterManager.setRenderer(new ClusterRenderer(this, map, clusterManager));
+            clusterManager.setOnClusterClickListener(this);
+            clusterManager.setOnClusterItemClickListener(this);
+            map.setOnCameraChangeListener(clusterManager);
+            map.setOnMarkerClickListener(clusterManager);
+
+            // User location
+            locationService = new LocationService(this, map, R.array.berlin);
+            locationService.zoomToRegion(false);
+        }
+
+        // Start synchronizing data
+        StolpersteineNetworkService networkService = new StolpersteineNetworkService(this);
+        networkService.getDefaultSearchData().setCity("Berlin");
+        synchronizationController = new SynchronizationController(networkService);
+        synchronizationController.setListener(this);
+        synchronizationController.synchronize();
+    }
+
+    private boolean isLocationOption() {
+        MenuItem menuItem = menu.findItem(R.id.action_positioning);
+        String locationTitle = getResources().getString(R.string.map_action_location);
+        return menuItem.getTitle().equals(locationTitle);
+    }
+
+    private void toggleOption() {
+        int title, drawable;
+        if (isLocationOption()) {
             title = R.string.map_action_region;
             drawable = R.drawable.ic_action_location_region;
-	    } else {
+        } else {
             title = R.string.map_action_location;
             drawable = R.drawable.ic_action_location_current;
-	    }
+        }
 
-	    MenuItem menuItem = menu.findItem(R.id.action_positioning);
-	    menuItem.setTitle(title);
-	    menuItem.setIcon(drawable);
-	}
-	
-	@Override
-	protected void onStart() {
-		super.onStart();
-		
-		locationService.start();
-	}
+        MenuItem menuItem = menu.findItem(R.id.action_positioning);
+        menuItem.setTitle(title);
+        menuItem.setIcon(drawable);
+    }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		
-		locationService.stop();
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.map, menu);
-		this.menu = menu;
-		
-		SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
-		final SearchView searchView = (SearchView)menu.findItem(R.id.search).getActionView();
-		searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-		SearchSuggestionProvider searchSuggestionProvider = (SearchSuggestionProvider)getContentResolver()
-		        .acquireContentProviderClient(SearchSuggestionProvider.AUTHORITY)
-		        .getLocalContentProvider();
-		searchSuggestionProvider.setNetworkService(synchronizationController.getNetworkService());
-		searchView.setOnSuggestionListener(new OnSuggestionListener() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        locationService.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationService.stop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.map, menu);
+        this.menu = menu;
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        SearchSuggestionProvider searchSuggestionProvider = (SearchSuggestionProvider) getContentResolver()
+                .acquireContentProviderClient(SearchSuggestionProvider.AUTHORITY)
+                .getLocalContentProvider();
+        searchSuggestionProvider.setNetworkService(synchronizationController.getNetworkService());
+        searchView.setOnSuggestionListener(new OnSuggestionListener() {
             @Override
             public boolean onSuggestionSelect(int position) {
                 return false;
@@ -128,13 +128,13 @@ public class MapActivity extends Activity implements SynchronizationController.L
                 return true;
             }
         });
-		
-		return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int itemId = item.getItemId();
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
         if (itemId == R.id.action_positioning) {
             if (isLocationOption()) {
                 locationService.zoomToCurrentLocation(16, true);
@@ -143,9 +143,9 @@ public class MapActivity extends Activity implements SynchronizationController.L
             }
             toggleOption();
         }
-        
-		return true;
-	}
+
+        return true;
+    }
 
     @Override
     public boolean onClusterClick(Cluster<Stolperstein> cluster) {
@@ -157,22 +157,22 @@ public class MapActivity extends Activity implements SynchronizationController.L
             onClusterItemClick(stolperstein);
         }
 
-    	return false;
+        return false;
     }
 
     @Override
     public boolean onClusterItemClick(Stolperstein stolperstein) {
         String bioUrl = stolperstein.getPerson().getBiographyUri().toString();
-    	startActivity(BioActivity.createIntent(this, bioUrl));
-    	
+        startActivity(BioActivity.createIntent(this, bioUrl));
+
         return false;
     }
-	
-	@Override
+
+    @Override
     public void onStolpersteineAdded(List<Stolperstein> stolpersteine) {
-	    if (clusterManager != null && stolpersteine != null) {
-	    	clusterManager.addItems(stolpersteine);
-	    	clusterManager.cluster();
-	    }
+        if (clusterManager != null && stolpersteine != null) {
+            clusterManager.addItems(stolpersteine);
+            clusterManager.cluster();
+        }
     }
 }
